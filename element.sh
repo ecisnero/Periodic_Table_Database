d3f0d1d0dfbbc5f720ff4c52d0c1543753618f5c
Erik Cisneros <eca.experimental@gmail.com>
2024-05-15 01:58:00 -0400

    feat: Add output for atomic number argument

--- element.sh
+++ element.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
+else
+  # If first argument is numeric
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    element=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE atomic_number=$1;")
+  fi
+  echo $element | while IFS='|' read atomic_number symbol name atomic_mass melting_point boiling_point type
+    do
+      echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+    done
 fi
