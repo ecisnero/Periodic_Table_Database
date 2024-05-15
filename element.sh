8b07fc5f0ffb0cddb65e1da4131d0f52b1d41a07
Erik Cisneros <eca.experimental@gmail.com>
2024-05-15 02:11:57 -0400

    fix: Add output for invalid output.

--- element.sh
+++ element.sh
@@ -13,11 +13,18 @@ else
   elif [[ ${#1} -le 2 ]]
   then
     element=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE symbol='$1';")
+  # If first argument is the name
   else
     element=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE name='$1';")
   fi
-  echo $element | while IFS='|' read atomic_number symbol name atomic_mass melting_point boiling_point type
-    do
-      echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
-    done
+  # If the element wasn't found
+  if [[ -z $element ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo $element | while IFS='|' read atomic_number symbol name atomic_mass melting_point boiling_point type
+      do
+        echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+      done
+  fi
 fi
