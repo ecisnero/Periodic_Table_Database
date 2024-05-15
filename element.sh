328c3c05f26dc4a56f3dc884bcc66636a47e4346
Erik Cisneros <eca.experimental@gmail.com>
2024-05-15 02:08:19 -0400

    feat: Add output for atomic name

--- element.sh
+++ element.sh
@@ -13,6 +13,8 @@ else
   elif [[ ${#1} -le 2 ]]
   then
     element=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE symbol='$1';")
+  else
+    element=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE name='$1';")
   fi
   echo $element | while IFS='|' read atomic_number symbol name atomic_mass melting_point boiling_point type
     do
